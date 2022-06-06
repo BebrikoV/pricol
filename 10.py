d730859e460a78c0111369185f56d3ff94f28dd1
BebrikoV <verglib08@gmail.com>
2022-06-06 19:24:00 +0300

    she odin commit

--- 10.py
+++ 10.py
@@ -2,7 +2,8 @@
 # opener = urllib.request.build_opener()
 # response = opener.open("https://httpbin.org/get")
 # print(response.read())
-import  requests
-response=requests.get("https://httpbin.org/get")
-print(response.content)
-print(f"Datatype - {type(response.content)}")
+import requests
+response = requests.post("https://httpbin.org/post", data="Prosto vesela Data", headers={"h1":"Test jakogos tutla"})
+
+print(response.text)
+
